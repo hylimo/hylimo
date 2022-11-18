a0a89082bb145fbe84292b0dec43561cdfc58284
nk-coding <niklask.coding@gmail.com>
2022-11-18 19:08:09 +0100

    webpack improvements

--- website/src/components/HylimoEditor/index.tsx
+++ website/src/components/HylimoEditor/index.tsx
@@ -1,12 +1,17 @@
 import { Allotment } from "allotment";
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { useColorMode } from "@docusaurus/theme-common";
 import "allotment/dist/style.css";
-import Editor from "@monaco-editor/react";
 import { customDarkTheme, customLightTheme, languageConfiguration, monarchTokenProvider } from "./language";
 import * as monaco from "monaco-editor";
-import { loader } from "@monaco-editor/react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import Loading from "@theme/Loading";
+import { defaultProps } from "prism-react-renderer";
+
+/**
+ * Monaco editor
+ */
+const MonacoEditor = lazy(() => import("react-monaco-editor"));
 
 /**
  * Name of the language
@@ -18,30 +23,39 @@ const language = "syncscript";
  * @returns the created editor component
  */
 export function HylimoEditor(): JSX.Element {
+    const { colorMode } = useColorMode();
     return (
         <BrowserOnly fallback={<div>Loading...</div>}>
             {() => {
-                useEffect(() => {
-                    loader.config({ monaco });
-                });
-                const { colorMode } = useColorMode();
                 return (
                     <Allotment>
                         <Allotment.Pane>
-                            <Editor
-                                theme={colorMode === "dark" ? "custom-dark" : "custom-light"}
-                                beforeMount={(editor) => {
-                                    editor.languages.register({ id: language });
-                                    editor.languages.setLanguageConfiguration(language, languageConfiguration as any);
-                                    editor.languages.setMonarchTokensProvider(
-                                        "syncscript",
-                                        monarchTokenProvider as any
-                                    );
-                                    editor.editor.defineTheme("custom-dark", customDarkTheme as any);
-                                    editor.editor.defineTheme("custom-light", customLightTheme as any);
-                                }}
-                                language="syncscript"
-                            ></Editor>
+                            <Suspense fallback={"Loading..."}>
+                                <MonacoEditor
+                                    options={{
+                                        automaticLayout: true
+                                    }}
+                                    theme={colorMode === "dark" ? "custom-dark" : "custom-light"}
+                                    editorWillMount={(editor) => {
+                                        console.log("this shit again")
+                                        editor.languages.register({ id: language });
+                                        editor.languages.setLanguageConfiguration(
+                                            language,
+                                            languageConfiguration as any
+                                        );
+                                        editor.languages.setMonarchTokensProvider(
+                                            "syncscript",
+                                            monarchTokenProvider as any
+                                        );
+                                        editor.editor.defineTheme("custom-dark", customDarkTheme as any);
+                                        editor.editor.defineTheme("custom-light", customLightTheme as any);
+                                    }}
+                                    editorWillUnmount={(editor) => {
+                                        
+                                    }}
+                                    language="syncscript"
+                                ></MonacoEditor>
+                            </Suspense>
                         </Allotment.Pane>
                         <Allotment.Pane>
                             <div>Hello world</div>
