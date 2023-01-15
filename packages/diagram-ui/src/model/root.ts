35d946a2661ea1a988b12d2f4b4ec0998f061768
nk-coding <niklask.coding@gmail.com>
2023-01-15 03:15:14 +0100

    remove duplicated code

--- packages/diagram-ui/src/model/root.ts
+++ packages/diagram-ui/src/model/root.ts
@@ -30,29 +30,6 @@ export class SRoot extends ViewportRootElement {
         g {
             --diagram-zoom: ${this.zoom};
         }
-        .selected-rect {
-            fill: var(--diagram-layout-color-overlay);
-            stroke: var(--diagram-layout-color-selected);
-            stroke-width: calc(5px / var(--diagram-zoom));
-            stroke-dasharray: calc(16px / var(--diagram-zoom));
-        }
-          
-        .canvas-point {
-            stroke-linecap: round;
-            stroke-width: calc(16px / var(--diagram-zoom));
-            stroke: var(--diagram-layout-color);
-        }
-          
-        .canvas-point.selected {
-            stroke: var(--diagram-layout-color-selected);
-        }
-          
-        .canvas-dependency-line {
-            stroke: var(--diagram-layout-color);
-            stroke-width: calc(2px / var(--diagram-zoom));
-            stroke-dasharray: calc(16px / var(--diagram-zoom));
-            fill: none;
-        }
         `;
         return (
             this.fonts
