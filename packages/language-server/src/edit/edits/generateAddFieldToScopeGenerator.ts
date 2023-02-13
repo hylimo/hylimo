89c7e4c2ef68811e6a06276776304b888b5a20d6
nk-coding <niklask.coding@gmail.com>
2023-02-13 05:56:22 +0100

    functional decomposition

--- packages/language-server/src/edit/edits/generateAddFieldToScopeGenerator.ts
+++ packages/language-server/src/edit/edits/generateAddFieldToScopeGenerator.ts
@@ -1,4 +1,4 @@
-import { FullObject, FunctionExpression } from "@hylimo/core";
+import { ASTExpressionPosition, FullObject, FunctionExpression } from "@hylimo/core";
 import { Position, Range, uinteger } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import { EditGeneratorEntry } from "../editGeneratorEntry";
@@ -25,61 +25,121 @@ export function generateAddFieldToScopeGenerator(
         if (!(scope instanceof FunctionExpression)) {
             throw new Error("scope must be a function expression");
         }
-        const position = scope.position!;
-        if (position.startLine == position.endLine) {
-            const innerText = document.getText(
-                Range.create(document.positionAt(position.startOffset + 1), document.positionAt(position.endOffset))
-            );
-            const innerTextWithoutWhitespace = innerText.replace(/^\s*/, "");
-            const indentation = extractIndentation(document, position.startLine - 1);
-            const innerIndentation = increaseIndentation(indentation);
-            let prefix: string;
-            if (innerTextWithoutWhitespace == "") {
-                prefix = "\n";
-            } else {
-                prefix = `\n${innerIndentation}${innerTextWithoutWhitespace}\n`;
-            }
-            return {
-                start: position.startOffset + 1,
-                end: position.endOffset,
-                generator: new FieldEntryGenerator(prefix, `\n${indentation}`, innerIndentation),
-                meta
-            };
-        } else {
-            const indentation = extractIndentation(document, position.startLine - 1);
-            const innerIndentation = increaseIndentation(indentation);
-            const lineEnd = document.getText(
-                Range.create(
-                    document.positionAt(position.startOffset + 1),
-                    Position.create(position.startLine - 1, uinteger.MAX_VALUE)
-                )
-            );
-            const lineEndContainsOnlyWhitespace = lineEnd.match(/^\s*$/);
-            const suffix = lineEndContainsOnlyWhitespace ? "" : `\n${innerIndentation}`;
-            return {
-                start: position.startOffset + 1,
-                end: position.startOffset + 1,
-                generator: new FieldEntryGenerator("\n", suffix, innerIndentation),
-                meta
-            };
-        }
+        return generateAddFieldToScope(scope, document, meta);
     } else {
-        const source = element.getLocalFieldOrUndefined("source")?.source;
-        if (source == undefined) {
-            throw new Error("element must have a source");
-        }
-        const indentation = extractIndentation(document, source.position!.startLine - 1);
-        return {
-            start: source.position!.endOffset + 1,
-            end: source.position!.endOffset + 1,
-            generator: new FieldEntryGenerator(
-                ` ${scopeName} {\n`,
-                `\n${indentation}}`,
-                increaseIndentation(indentation)
-            ),
-            meta
-        };
+        return generateCreateNewScope(element, scopeName, document, meta);
     }
+}
+
+/**
+ * Creates an EditGeneratorEntry to add fields to an already existing function scope.
+ * Handles all possible cases how the function block is structured and tries to preserve formatting in common cases.
+ *
+ * @param scope the function scope where to add the fields
+ * @param document the document in which the function scope is located
+ * @param meta metadata to add to the generated entry
+ * @returns the generated entry
+ */
+function generateAddFieldToScope(scope: FunctionExpression, document: TextDocument, meta: any): EditGeneratorEntry {
+    const position = scope.position!;
+    if (position.startLine == position.endLine) {
+        return generateAddFieldToScopeReplaceInner(position, document, meta);
+    } else {
+        return generateAddFieldToScopeInsertAtFirstLine(position, document, meta);
+    }
+}
+
+/**
+ * Creates an EditGeneratorEntry to add fields to a scope by creating a new scope with the given name and inserting the fields there.
+ *
+ * @param element the element where to add the fields
+ * @param scopeName the name of the scope where to add the fields
+ * @param document the document in which the element is located
+ * @param meta metadata to add to the generated entry
+ * @returns the generated entry
+ */
+function generateCreateNewScope(
+    element: FullObject,
+    scopeName: string,
+    document: TextDocument,
+    meta: any
+): EditGeneratorEntry {
+    const source = element.getLocalFieldOrUndefined("source")?.source;
+    if (source == undefined) {
+        throw new Error("element must have a source");
+    }
+    const indentation = extractIndentation(document, source.position!.startLine - 1);
+    return {
+        start: source.position!.endOffset + 1,
+        end: source.position!.endOffset + 1,
+        generator: new FieldEntryGenerator(` ${scopeName} {\n`, `\n${indentation}}`, increaseIndentation(indentation)),
+        meta
+    };
+}
+
+/**
+ * Creates an EditGeneratorEntry to add fields to a scope by replacing the inner text of the function block
+ * and moving it to the next line.
+ *
+ * @param position the start position of the function block
+ * @param document the document in which the function block is located
+ * @param meta metadata to add to the generated entry
+ * @returns the generated entry
+ */
+function generateAddFieldToScopeReplaceInner(
+    position: ASTExpressionPosition,
+    document: TextDocument,
+    meta: any
+): EditGeneratorEntry {
+    const innerText = document.getText(
+        Range.create(document.positionAt(position.startOffset + 1), document.positionAt(position.endOffset))
+    );
+    const innerTextWithoutWhitespace = innerText.replace(/^\s*/, "");
+    const indentation = extractIndentation(document, position.startLine - 1);
+    const innerIndentation = increaseIndentation(indentation);
+    let prefix: string;
+    if (innerTextWithoutWhitespace == "") {
+        prefix = "\n";
+    } else {
+        prefix = `\n${innerIndentation}${innerTextWithoutWhitespace}\n`;
+    }
+    return {
+        start: position.startOffset + 1,
+        end: position.endOffset,
+        generator: new FieldEntryGenerator(prefix, `\n${indentation}`, innerIndentation),
+        meta
+    };
+}
+
+/**
+ * Creates an EditGeneratorEntry to add fields to a scope by inserting the fields at the first line of the function block.
+ *
+ * @param position the start position of the function block
+ * @param document the document in which the function block is located
+ * @param meta metadata to add to the generated entry
+ * @returns the generated entry
+ */
+function generateAddFieldToScopeInsertAtFirstLine(
+    position: ASTExpressionPosition,
+    document: TextDocument,
+    meta: any
+): EditGeneratorEntry {
+    const indentation = extractIndentation(document, position.startLine - 1);
+    const innerIndentation = increaseIndentation(indentation);
+    const lineEnd = document.getText(
+        Range.create(
+            document.positionAt(position.startOffset + 1),
+            Position.create(position.startLine - 1, uinteger.MAX_VALUE)
+        )
+    );
+    const lineEndContainsOnlyWhitespace = lineEnd.match(/^\s*$/);
+    const suffix = lineEndContainsOnlyWhitespace ? "" : `\n${innerIndentation}`;
+    return {
+        start: position.startOffset + 1,
+        end: position.startOffset + 1,
+        generator: new FieldEntryGenerator("\n", suffix, innerIndentation),
+        meta
+    };
 }
 
 /**
