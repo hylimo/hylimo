75c6c0e49fcb90a94ee7e56b963bf4897a269b19
Niklas Krieger <53957498+nk-coding@users.noreply.github.com>
2024-08-25 01:42:31 +0200

    Update packages/diagram-common/src/model/elements/defaultEditTypes.ts
    Co-authored-by: delvh <dev.lh@web.de>

--- packages/diagram-common/src/model/elements/defaultEditTypes.ts
+++ packages/diagram-common/src/model/elements/defaultEditTypes.ts
@@ -5,13 +5,13 @@ export enum DefaultEditTypes {
     MOVE_X = "update/move/x",
     MOVE_Y = "update/move/y",
 
-    MOVE_LPOS_POS = "update/moveLPos/pos",
-    MOVE_LPOS_DIST = "update/moveLPos/dist",
+    MOVE_LPOS_POS = "update/move-lpos/pos",
+    MOVE_LPOS_DIST = "update/move-lpos/dist",
 
     RESIZE_WIDTH = "update/resize/width",
     RESIZE_HEIGHT = "update/resize/height",
 
-    AXIS_ALIGNED_SEGMENT_POS = "update/axisAlignedSegment/pos",
+    AXIS_ALIGNED_SEGMENT_POS = "update/axis-aligned-segment/pos",
 
     ROTATE = "update/rotate"
 }
