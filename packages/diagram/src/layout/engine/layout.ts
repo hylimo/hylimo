f90cd1ce3cfb11b28984889c96246f030f14854d
Niklas Krieger <53957498+nk-coding@users.noreply.github.com>
2024-09-10 05:45:38 +0200

    Update packages/diagram/src/layout/engine/layout.ts
    Co-authored-by: delvh <dev.lh@web.de>

--- packages/diagram/src/layout/engine/layout.ts
+++ packages/diagram/src/layout/engine/layout.ts
@@ -73,11 +73,8 @@ export class Layout {
             return element.class.has(selector.value);
         } else if (selector.type === SelectorType.TYPE) {
             return element.layoutConfig.type === selector.value;
-        } else if (selector.type === SelectorType.ANY) {
-            return true;
-        } else {
-            return false;
-        }
+        } 
+        return selector.type === SelectorType.ANY;
     }
 
     /**
