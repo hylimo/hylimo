3eb5109e1534be2a4e6db65f9279983b178ba7c3
nk-coding <niklask.coding@gmail.com>
2024-09-11 12:56:02 +0200

    add more docs

--- packages/diagram-common/src/model/elements/canvas/marker.ts
+++ packages/diagram-common/src/model/elements/canvas/marker.ts
@@ -4,12 +4,16 @@ import { SizedElement } from "../base/sizedElement.js";
 
 /**
  * Marker which can be placed at the start or end of a CanvasConnection
+ * The marker is layouted in a way that the center of the right side of
+ * the marker is located where the line would regularly start or end.
+ * Depending on the lineStart value, the line then actually starts at some point on the horizontal center line of the marker.
  */
-
 export interface Marker extends SizedElement {
     type: typeof Marker.TYPE;
     /**
-     * The position on the vertical center line where the line actually starts
+     * The position on the horizontal center line where the line actually starts.
+     * E.g. 1 meaning the line starts at the left edge of the marker, thus the line is shortend by the width of the marker,
+     * while 0 means the line starts at the right edge of the marker, thus the line not being shortened at all.
      */
     lineStart: number;
     /**
