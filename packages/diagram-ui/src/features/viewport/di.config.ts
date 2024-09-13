2197b6c167b6a578eeae279f42439d82cd5d8f0a
delvh <leon@kske.dev>
2024-09-13 15:48:06 +0200

    Fit the diagram to viewport on initial load

--- packages/diagram-ui/src/features/viewport/di.config.ts
+++ packages/diagram-ui/src/features/viewport/di.config.ts
@@ -1,11 +1,14 @@
 import { ContainerModule } from "inversify";
-import { TYPES } from "sprotty";
+import { configureActionHandler, TYPES } from "sprotty";
 import { ViewportTouchListener } from "./touch.js";
+import { SetModelAction } from "sprotty-protocol";
+import { SetModelActionHandler } from "./setModelActionHandler.js";
 
 /**
  * Module which configures the ViewportTouchListener
  */
-export const viewportModule = new ContainerModule((bind) => {
+export const viewportModule = new ContainerModule((bind, _, isBound) => {
     bind(ViewportTouchListener).toSelf().inSingletonScope();
     bind(TYPES.IVNodePostprocessor).toService(ViewportTouchListener);
+    configureActionHandler({ bind, isBound }, SetModelAction.KIND, SetModelActionHandler);
 });
