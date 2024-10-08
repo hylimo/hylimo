38e1df7ec5bf8374b8e29f85985e8ebae7b0dc09
delvh <leon@kske.dev>
2024-10-08 13:32:47 +0200

    Do not animate `fit to screen` on model load

--- packages/diagram-ui/src/features/viewport/fitToScreenAction.ts
+++ packages/diagram-ui/src/features/viewport/fitToScreenAction.ts
@@ -2,7 +2,9 @@ import { FitToScreenAction } from "sprotty-protocol";
 
 /**
  * Creates a new action to fit the viewport to the current diagram with common params.
+ *
+ * @param animate whether to animate the action
  */
-export function createFitToScreenAction(): FitToScreenAction {
-    return FitToScreenAction.create([], { padding: 50, maxZoom: 2 });
+export function createFitToScreenAction(animate: boolean | undefined = undefined): FitToScreenAction {
+    return FitToScreenAction.create([], { padding: 50, maxZoom: 2, animate });
 }
