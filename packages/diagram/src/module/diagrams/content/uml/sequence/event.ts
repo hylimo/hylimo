e9c73e8775fa254840eea6d93c94a82e076a5fae
delvh <leon@kske.dev>
2024-10-27 23:05:16 +0100

    Avoid lifelines ending seemingly "too early"
    If we only increase a lifeline by 2 times the margin, the "hole" between subsequent
    strokes can be large enough that no next stroke is drawn, making it look
    like the last event is the immediate end of the diagram without any
    margin

--- packages/diagram/src/module/diagrams/content/uml/sequence/event.ts
+++ packages/diagram/src/module/diagrams/content/uml/sequence/event.ts
@@ -41,9 +41,9 @@ export const eventModule = InterpreterModule.create(
                     it.events += eventPosition // for the position calculation
                     eventObject[it.name] = eventPosition // for arrows, i.e. 'event.shop --> event.cart'
                     
-                    // change the length of the instance line (its end position) to the new last position + 2*margin
-                    // (+2 margin so that a lifeline that is still present there can end, and there's still a bit of the line left over)
-                    endpos = scope.rpos(eventPosition, 0, 2*scope.margin)
+                    // change the length of the instance line (its end position) to the new last position + 3*margin
+                    // (+3 margin so that a lifeline that is still present there can end, and there's still a bit of the line left over)
+                    endpos = scope.rpos(eventPosition, 0, 3*scope.margin)
                     it.line.contents.get(it.line.contents.length - 1).end = endpos
                     println(endpos)
 
