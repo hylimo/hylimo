6e2be228263e61e4e52d97956ffe36f5a4cac57c
delvh <leon@kske.dev>
2024-12-13 16:58:02 +0100

    When destroying a participant, make sure to trim the lifeline to the cross

--- packages/diagram/src/module/diagrams/content/uml/sequence/participant.ts
+++ packages/diagram/src/module/diagrams/content/uml/sequence/participant.ts
@@ -116,6 +116,9 @@ export const participantModule = InterpreterModule.create(
                 // We must change the position by '2*margin' compared to the last event as the lifeline continues to be drawn by two additional margins
                 cross.pos = scope.rpos(participant.events.get(participant.events.length - 1), 0, 2*scope.margin)
                 scope.internal.registerCanvasElement(cross, originalArgs, originalArgs.self)
+
+                // Also shorten the lifeline if necessary
+                participant.lifeline.contents.get(participant.lifeline.contents.length - 1).end = cross.pos
               }
               `,
                 {
