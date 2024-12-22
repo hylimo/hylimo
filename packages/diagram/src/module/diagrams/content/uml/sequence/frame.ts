43e1efd1ca959c6daef6350c4b6010ab9fc0fe63
delvh <leon@kske.dev>
2024-12-22 22:46:00 +0100

    Prepare support for fragments
    In the following commits, I'll have to find out how to use args for
    `callWithScope` and implement the logic for the main fragment display.

--- packages/diagram/src/module/diagrams/content/uml/sequence/frame.ts
+++ packages/diagram/src/module/diagrams/content/uml/sequence/frame.ts
@@ -1,6 +1,8 @@
-import { booleanType, fun, id, InterpreterModule, optional, stringType } from "@hylimo/core";
+import { booleanType, fun, functionType, id, InterpreterModule, optional, or, stringType } from "@hylimo/core";
 import { SCOPE } from "../../../../base/dslModule.js";
-import { eventCoordinateType } from "./types.js";
+import { eventCoordinateType, participantType } from "./types.js";
+
+const operatorRectangleSVG = "M 0 0 H 20 V 5 L 16 9 H 0 V 0";
 
 /**
  * Defines a frame.<br>
@@ -18,12 +20,14 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
           scope.internal.createSequenceDiagramFragment = {
             (topLineEvent) = args
             if(topLineEvent == null) {
-                scope.error("No event (coordinate) was passed to 'fragment'")
+              scope.error("No event (coordinate) was passed to 'fragment'")
             }
+            
+            parentFrame = args.parentFrame
             
             // Best effort guess to see if the given event is either an event or an event coordinate
             if((topLineEvent.name == null) && (topLineEvent.participantName == null) || (topLineEvent.y.proto != 1.proto)) {
-                scope.error("The event passed to 'fragment' is neither an event nor an event coordinate")
+              scope.error("The event passed to 'fragment' is neither an event nor an event coordinate")
             }
             
             parentLeftX = args.parentLeftXCoordinate
@@ -32,7 +36,12 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
             width = parentRightX - parentLeftX
             
             // TODO: Finish from here on
-            separatingLineElement = canvasElement(content = path())
+            separatingLine = canvasElement(
+              content = path(path = "H 1", class = list("fragment-line")),
+              class = list("fragment-line-element"))
+            separatingLine.pos = scope.apos()
+
+            scope.internal.registerCanvasElement(frameElement, args, args.self)
             
             
           }
@@ -43,9 +52,14 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
               scope.error("createSequenceDiagramFrame is missing its 'args = args' parameter")
             }
             args = args.args
+            argsCopy = args
             
             (fragmentFunction) = args
             fragmentFunction = fragmentFunction ?? {}
+            
+            text = args.text
+            hasIcon = args.hasIcon ?? (text != null)
+            subtext = args.subtext
 
             // We must differentiate four different version to set margin:
             // 1. No margin set -> use scope.frameMarginX/Y
@@ -74,16 +88,51 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
             width = bottomRight.x - topLeft.x + marginLeft + marginRight
             height = bottomRight.y - topLeft.y + marginTop + marginBottom
             
+            
             // Add the frame
             frameElement = canvasElement(
-                    content = rect(class = list("frame")),
-                    class = list("frame-element"),
-                    width = width,
-                    height = height
-                )
+              content = rect(class = list("frame")),
+                class = list("frame-element"),
+                width = width,
+                height = height
+              )
+            frameElement.topLeft = topLeft
+            frameElement.bottomRight = bottomRight
+            frameElement.fragments = list()
+            frameElement.text = text
+            frameElement.hasIcon = icon
+            frameElement.subtext = subtext
+ 
             frameElement.pos = scope.apos(x, y)
  
             scope.internal.registerCanvasElement(frameElement, args, args.self)
+ 
+            // Calculate the auxilliary parts of the frame - first the 'nearly' rectangle in the upper left corner
+            if(hasIcon) {
+              operatorRectangleElement = canvasElement(content = 
+                path(path = "${operatorRectangleSVG}", class = list("operator-rectangle")),
+                class = list("operator-rectangle-element"))
+              upperLeftCornerElement.pos = scope.apos(x, y)
+              scope.internal.registerCanvasElement(operatorRectangleElement, argsCopy, argsCopy.self)
+            }
+            
+            scope.styles {
+              cls("operator-rectangle") {
+                
+              }
+            }
+            
+            // Then the frame text
+            if(text != null) {
+            }
+            
+            // And the subtext if present
+            if(subtext != null) {
+            } 
+            
+            // Lastly, generate all fragments for the given frame
+            fragmentFunction.callWithScope([parent = frameElement, fragment = scope.internal.createSequenceDiagramFragment ])
+            
             frameElement
           }
         `,
@@ -96,6 +145,11 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
                 {
                     docs: "Creates a frame around two endpoints",
                     params: [
+                        [
+                            0,
+                            "A function generating all fragments (additional compartments within the frame)",
+                            optional(functionType)
+                        ],
                         ["text", "The text to display in the upper-left corner", optional(stringType)],
                         [
                             "subtext",
@@ -104,18 +158,18 @@ export const sequenceDiagramFrameModule = InterpreterModule.create(
                         ],
                         [
                             "hasIcon",
-                            "Whether to draw the UML border around the text. If false, only the outer boundary will be drawn",
+                            "Whether to draw the UML border around the text. If false, only the outer boundary will be drawn. Will be shown by default when the frame should have text",
                             optional(booleanType)
                         ],
                         [
                             "topLeft",
                             "The top-left coordinate (event) to draw the border around. The border will be extended by 'frameMargin' on each side",
-                            eventCoordinateType
+                            or(eventCoordinateType, participantType)
                         ],
                         [
                             "bottomRight",
                             "The bottom-right coordinate (event) to draw the border around. The border will be extended by 'frameMargin' on each side",
-                            eventCoordinateType
+                            or(eventCoordinateType, participantType)
                         ]
                     ],
                     snippet: `(topLeft = $1, bottomRight = $1)`,
