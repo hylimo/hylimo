b49aea0691b2fa44e3843da73c341484103be679
Niklas Krieger <niklask.coding@gmail.com>
2025-06-27 01:00:42 +0200

    fix custom nav bar item

--- website/.vitepress/components/CustomNavBarMenuLink.vue
+++ website/.vitepress/components/CustomNavBarMenuLink.vue
@@ -1,16 +1,22 @@
 <template>
+    <VPNavScreenMenuLink v-if="screenMenu" :item="transformedItem" />
     <VPNavBarMenuLink :item="transformedItem" />
 </template>
 <script setup lang="ts">
 import { useUrlSearchParams } from "@vueuse/core";
 import type { DefaultTheme } from "vitepress";
 import VPNavBarMenuLink from "vitepress/dist/client/theme-default/components/VPNavBarMenuLink.vue";
+import VPNavScreenMenuLink from "vitepress/dist/client/theme-default/components/VPNavScreenMenuLink.vue";
 import { computed, type PropType } from "vue";
 
 const props = defineProps({
     item: {
         type: Object as PropType<DefaultTheme.NavItemWithLink>,
         required: true
+    },
+    screenMenu: {
+        type: Boolean,
+        default: false
     }
 });
 
